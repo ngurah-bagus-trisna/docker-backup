ed81713c5f73baa1f86ed92f39afc25eab74314e
ngurah-bagus-trisna <ngurahbagusnb391@gmail.com>
2026-01-11 15:29:27 +0700

    docs: Add some comment about rclone/gdrive backup on config.sh

--- config.sh
+++ config.sh
@@ -2,11 +2,16 @@
 # config.sh
 
 # Path lokal untuk staging sebelum dikirim ke Restic
-BACKUP_ROOT="/backup"
+BACKUP_ROOT="/tmp/backup"
 TMP_DIR="$BACKUP_ROOT/tmp"
 LOG_DIR="$BACKUP_ROOT/logs"
 
 # Restic Config
+
+# If using Rclone/GDrive as backend
+# export RESTIC_REPOSITORY="rclone:<your-rclone-config>:backup-repo"
+
+# If using local backup
 export RESTIC_REPOSITORY="$BACKUP_ROOT/restic"
 export RESTIC_PASSWORD_FILE="/etc/restic.pass"
 
